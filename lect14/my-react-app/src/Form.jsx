691732bc64438cd419967ec8eb43c854fd2173a8
charu-159 <charupatel159@gmail.com>
2026-05-27 11:32:58 +0530

    form

--- lect14/my-react-app/src/Form.jsx
+++ lect14/my-react-app/src/Form.jsx
@@ -1,19 +1,48 @@
 
 
-import React from "react";
 
-function Form() {
+import React, { useState } from 'react'
+import './App.css'
+const Form = () => {
+      let [input,SetInput]=      useState(
+        {name:"",email:"",passWord:""})
+
+
+    function fun1(e){
+        // console.log("hello");
+       let {name,value}=    e.target
+       SetInput({...input,[name]:value})
+        
+        console.log(input);
+        
+        
+        // console.log(e.target.value);
+        
+        
+    }
   return (
-    <div className="box">
-      <input type="text" placeholder="Enter your name" />
+    <div>
+        <form>
+            <input type='text' name='name' value={input.name}  onChange={fun1} placeholder='Enter your name'/>
+            <br></br>
+            <br></br>
 
-      <input type="email" placeholder="Enter your email" />
+            <input type='email'   name='email' value={input.email}  onChange={fun1} placeholder='Enter your email'/>
+            <br></br>
+            <br></br>
 
-      <input type="password" placeholder="Enter your password" />
+            <input type='email'  name='passWord' value={input.passWord}  onChange={fun1} placeholder='Enter your email'/>
+            <br></br>
+            <br></br>
 
-      <button>Add</button>
+            
+            <button>submit</button>
+
+
+
+        </form>
     </div>
-  );
+  )
 }
 
-export default Form;
+export default Form
